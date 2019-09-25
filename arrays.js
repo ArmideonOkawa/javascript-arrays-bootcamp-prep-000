04a8ef4af907ddef1567d5dcd7488cd57c348996
jeremy perry <jeremy.m.perry@gmail.com>
2019-09-25 01:44:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,6 +16,6 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.pop(element);
+  array.push(element);
   return array;
 }
