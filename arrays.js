5e4de7b6c0710fa347299621bf3b7026ef2d45f9
jeremy perry <jeremy.m.perry@gmail.com>
2019-09-25 01:02:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return newArr;
+  return array.unshift(element);
+  
 }
 
 function destructivelyAddElementsToBeginningOfArray(array, element){
